0b512043553c19bafa38281b1a5cb6dac368e777
Stanislav Příhoda <stanislav.prihoda@gmail.com>
2019-02-10 10:21:46 +0100

    repo consolidation

--- Docker_and_Kubernetes/simple-web/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/',(req,res) => {
-    res.send('Bye there');
-});
-app.listen(8080, () => {
-    console.log('Listening on port 8080');
-});
