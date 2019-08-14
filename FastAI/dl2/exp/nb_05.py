cf6d7fe9022a59db44c72eac8248bdd6b79a2c57
Stanislav Příhoda <stanislav.prihoda@gmail.com>
2019-08-14 09:13:50 +0200

    fastai part2

--- FastAI/dl2/exp/nb_05.py
+++ FastAI/dl2/exp/nb_05.py
@@ -11,6 +11,36 @@ def create_learner(model_func, loss_func, data):
 
 def get_model_func(lr=0.5): return partial(get_model,lr=lr)
 
+# added based on fastai exp
+class Recorder(Callback):
+    def begin_fit(self):
+        self.lrs = [[] for _ in self.opt.param_groups]
+        self.losses = []
+
+    def after_batch(self):
+        if not self.in_train: return
+        for pg,lr in zip(self.opt.param_groups,self.lrs): lr.append(pg['lr'])
+        self.losses.append(self.loss.detach().cpu())
+
+    def plot_lr  (self, pgid=-1): plt.plot(self.lrs[pgid])
+    def plot_loss(self, skip_last=0): plt.plot(self.losses[:len(self.losses)-skip_last])
+
+class ParamScheduler(Callback):
+    _order=1
+    def __init__(self, pname, sched_funcs): self.pname,self.sched_funcs = pname,sched_funcs
+
+    def begin_fit(self):
+        if not isinstance(self.sched_funcs, (list,tuple)):
+            self.sched_funcs = [self.sched_funcs] * len(self.opt.param_groups)
+
+    def set_param(self):
+        assert len(self.opt.param_groups)==len(self.sched_funcs)
+        for pg,f in zip(self.opt.param_groups,self.sched_funcs):
+            pg[self.pname] = f(self.n_epochs/self.epochs)
+
+    def begin_batch(self):
+        if self.in_train: self.set_param()
+
 def annealer(f):
     def _inner(start,end): return partial(f,start,end)
     return _inner
@@ -24,6 +54,9 @@ def sched_cos(start,end,pos):return start+(1+math.cos(math.pi*(1-pos)))*(end-sta
 def sched_no(start,end,pos):return start
 @annealer
 def sched_exp(start,end,pos): return start*(end/start)**pos
+
+#this money-patch is there to be able to plot tensors
+torch.Tensor.ndim=property(lambda x: len(x.shape))
 
 def combine_scheds(pcts, scheds):
     assert sum(pcts) == 1.
